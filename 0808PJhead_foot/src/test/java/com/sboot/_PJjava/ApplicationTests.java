4461c9bbced5253a7a5ac065db0e145f6aaef4a0
DESKTOP-3SLL5AM\user <hascience80@gmail.com>
2025-08-08 17:22:37 +0900

    header, footer 1차 디자인

---
+++ 0808PJhead_foot/src/test/java/com/sboot/_PJjava/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sboot._PJjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
