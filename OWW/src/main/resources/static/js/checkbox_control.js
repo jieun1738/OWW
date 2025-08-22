f3ea7ed910bc46bb13d7d5d015c73e77b8f86fa4
jieun1738 <cochun1738@naver.com>
2025-08-22 13:34:37 +0900

    3rd-dashboard 완성

--- OWW/src/main/resources/static/js/checkbox_control.js
+++ OWW/src/main/resources/static/js/checkbox_control.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		const contractCheckbox = document.getElementById(`contract_${category}_ch`);
 		const payCheckbox = document.getElementById(`pay_${category}`);
 		const buttons = document.querySelectorAll(`.${category} .btn, .${category} .payBtn`);
+		const total_pay_btn = document.querySelectorAll(`.total .payBtn`);
 
 		if (!payCheckbox || !contractCheckbox) return;
 
@@ -12,8 +13,14 @@ document.addEventListener('DOMContentLoaded', () => {
 		function toggleDisabled() {
 			if (payCheckbox.checked) {
 				buttons.forEach(btn => btn.disabled = true);
+				total_pay_btn.forEach(btn => btn.disabled = true);
+				contractCheckbox.checked = true;
 				contractCheckbox.disabled = true;
 				payCheckbox.disabled = true; // 체크 해제 방지
+			}else {
+			    buttons.forEach(btn => btn.disabled = false);
+				total_pay_btn.forEach(btn => btn.disabled = false);
+			    contractCheckbox.disabled = false;
 			}
 		}
 
@@ -21,16 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		// 초기 로드 시 확인
 		toggleDisabled();
 
-		// 체크박스 클릭 이벤트 (체크 해제 방지용)
-		payCheckbox.addEventListener('click', (e) => {
-			if (payCheckbox.checked) {
-				e.preventDefault(); // 체크 해제 방지
-			}
-		});
-		contractCheckbox.addEventListener('click', (e) => {
-			if (contractCheckbox.checked) {
-				e.preventDefault(); // 체크 해제 방지
-			}
-		});
+		// payCheckbox 상태 변경 시마다 적용
+		payCheckbox.addEventListener('change', toggleDisabled);
 	});
 	});
