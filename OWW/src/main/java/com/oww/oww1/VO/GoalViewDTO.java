5b56e62c992e543df4d4aca85700542f94866212
sung-min <dbs0877@gmail.com>
2025-08-22 19:48:27 +0900

    주석 달아놓은 safeboxController부터 DB작업 하시면 됩니다. 화이핑

---
+++ OWW/src/main/java/com/oww/oww1/VO/GoalViewDTO.java
@@ -0,0 +1,12 @@
+package com.oww.oww1.VO;
+
+import lombok.Data;
+
+@Data
+public class GoalViewDTO {
+	public long targetAmount;
+	public long savedAmount;
+	public boolean regular;
+
+
+}
--- OWW/src/main/java/com/oww/oww1/controller/SafeboxController.java
+++ OWW/src/main/java/com/oww/oww1/controller/SafeboxController.java
@@ -3,6 +3,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.oww.oww1.VO.GoalViewDTO;
+
 @Controller
 public class SafeboxController {
 
@@ -10,30 +12,25 @@ public class SafeboxController {
 	/*
 	 * @GetMapping("/") public String home() { return "redirect:/mypage/safe_box"; }
 	 */
-
     // 금고 화면
     @GetMapping("/mypage/safe_box")
     public String safeBox(Model model){
-        model.addAttribute("goal", new GoalView(10_000_000L, 7_000_000L, true));
+    	GoalViewDTO gv = new GoalViewDTO();
+    	// 고정값 이므로 DB연결지점은 여기입니다.
+    	gv.setTargetAmount(10_000_000L);
+    	gv.setSavedAmount(7_000_000L);
+    	gv.setRegular(true);
+        model.addAttribute("goal", gv);
         return "safe/safe_box";
     }
 
 
+	@GetMapping("/hello")
+    public String hello(){ 
+    	return "hello"; 
+    	}
+
+
+
     
-    @GetMapping("/hello")
-    public String hello(){ return "hello"; }
-
-
-    // 화면 바인딩용 간단 DTO
-    public static class GoalView {
-        public long targetAmount;
-        public long savedAmount;
-        public boolean regular;
-
-        public GoalView(long targetAmount, long savedAmount, boolean regular) {
-            this.targetAmount = targetAmount;
-            this.savedAmount = savedAmount;
-            this.regular = regular;
-        }
-    }
 }
