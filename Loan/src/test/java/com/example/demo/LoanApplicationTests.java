cad2782cbc4b9bedd899a7fe231cce3401e5feb7
DWOONIPC\wldms <cochun1738@naver.com>
2025-08-23 19:41:13 +0900

    loan+css 1차

--- Loan/src/test/java/com/example/demo/LoanApplicationTests.java
+++ Loan/src/test/java/com/example/demo/LoanApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.loan.LoanApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = LoanApplication.class)
 class LoanApplicationTests {
 
 	@Test
